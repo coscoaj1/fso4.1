2c35efd12c58142cb64d03517008e72418103555
Anthony Cosco <coscoanthony1@gmail.com>
2021-08-31 20:10:30 -0400

    added comment routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const commentsRouter = require('./controllers/comments');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -35,6 +36,7 @@ app.use(middleware.tokenExtractor);
 app.use('/api/login', loginRouter);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/blogs/', commentsRouter);
 
 if (process.env.NODE_ENV === 'test') {
 	const testRouter = require('./controllers/testing');
